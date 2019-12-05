fdad979ed1bbbb1e70036caff06358c5ec90dc04
youlai <761364115@qq.com>
2019-12-05 11:49:16 +0800

    Initial Commit

---
+++ bg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bg
+//
+//  Created by Apple on 2019/12/5.
+//  Copyright © 2019 zhkj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
